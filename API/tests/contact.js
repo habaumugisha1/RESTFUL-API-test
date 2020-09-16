e55c23839fc112e43cf0fe93aaa7f2b7ede10265
habaumugisha1 <habajeunes2@gmail.com>
2020-09-16 07:32:40 +0200

    every user can contacts website owner

---
+++ API/tests/contact.js
@@ -0,0 +1,69 @@
+// import express from " express";
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+import server from '../../index'
+import mongoose from 'mongoose'
+import Contacts from "../models/contact";
+
+chai.should()
+chai.use(chaiHttp)
+// clea up database
+
+describe('Contacts', () => {
+    // beforeEach( done => {
+    //     Contacts.remove({}, (err) =>{
+        
+    //         done();
+    //     })
+    // })
+
+    // testing create contacts message
+    describe('/api/v1/newContact', ()=>{
+        it('It should not create contacts message without name', (done) =>{
+            let contact = {
+                email: "habajeunes2@gmail.com",
+                content :"nice tests"
+            }
+            let conta = {}
+            chai.request(server)
+                .post('/api/v1/newContact')
+                .send(contact)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                done();
+
+                })
+        });
+
+        it('It should not create contacts message without input data', (done) =>{
+            
+            let conta = {}
+            chai.request(server)
+                .post('/api/v1/newContact')
+                .send(conta)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                done();
+
+                })
+        });
+
+        it('It should create contact message with input data', (done) =>{
+            
+            let contactData = {
+                name:"ami",
+                email: "habajeunes2@gmail.com",
+                content :"nice tests"
+            }
+            chai.request(server)
+                .post('/api/v1/newContact')
+                .send(contactData)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.a('object');
+                    
+                })
+                done();
+        })
+    })
+})
--- index.js
+++ index.js
@@ -5,25 +5,24 @@ import "dotenv/config"
 import myRouter from './API/router/routes'
 
 
+const app = express()
 const PORT = process.env.PORT
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 
 const url = process.env.DB_CONFIG;
+app.use('/api/v1', myRouter)
+
 mongoose.connect( url, { useNewUrlParser: true,  useUnifiedTopology: true}).then( () =>{
     console.log("connected to mongodb")
-    const app = express()
+    
+});
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:false}))
-    
-    //app.use('/books', bookRouter)
-    app.use('/api/v1', myRouter)
-    
-    app.listen( PORT, ()=> {
-        console.log(`server is running on ${PORT}...`)
-    })
+app.listen( PORT, ()=> {
+    console.log(`server is running on ${PORT}...`)
 })
 
-
+export default app;
 
 
 
