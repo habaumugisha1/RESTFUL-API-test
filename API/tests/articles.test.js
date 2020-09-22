0d4c7e57d45d1e449d7c16e0d0a135034cb3aa8e
andela <andela@andelas-MacBook-Pro.local>
2020-09-22 12:34:27 +0200

    resolve promise

---
+++ API/tests/articles.test.js
@@ -0,0 +1,121 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import fs from 'fs'
+import app from '../../index'
+import damyData from './damyData'
+
+chai.use(chaiHttp)
+
+describe( "This is article", ()=>{
+it("should not create article if no title provided", (done) => {
+    chai.request(app)
+    .post('/api/v1/newArticle')
+    .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+    .send({articleImage:"sdhfhdfsfjasfs",
+    description:"hfajshfdasdfvlajsfv"})
+    .end((error, res) => {
+        expect(res).to.have.status([400]);
+        done(error)
+    })
+})
+it("should not create article if no user is not admin", (done) => {
+    chai.request(app)
+    .post('/api/v1/newArticle')
+    .set('Authorization', `Bearer ${damyData.userToken}`)
+    .send({articleImage:"sdhfhdfsfjasfs",
+    description:"hfajshfdasdfvlajsfv"})
+    .end((error, res) => {
+        expect(res).to.have.status([403]);
+        done(error)
+    })
+})
+it("should not create article if no user is no token provided", (done) => {
+    chai.request(app)
+    .post('/api/v1/newArticle')
+    .set('Authorization', `Bearer ${damyData.emptyToken}`)
+    .send({articleImage:"sdhfhdfsfjasfs",
+    description:"hfajshfdasdfvlajsfv"})
+    .end((error, res) => {
+        expect(res).to.have.status([400]);
+        done(error)
+    })
+})
+it('It should not create article if no error', (done) =>{
+            
+            chai.request(app)
+                .post('/api/v1/newArticle')
+                .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+                .set('Content-Type', 'multipart/form-data')
+                .field('title', 'franklin')
+                .attach('articleImage',
+        fs.readFileSync('images/how I get into Andela.jpg'))
+                .field('description', 'testing files')
+                .end((error, res) => {
+                    expect(res).to.have.status([201])
+                done(error);
+
+                })
+        });
+
+        it('It should get all articles', (done) =>{
+            
+            chai.request(app)
+                .get('/api/v1/blogs')
+                
+                .end((error, res) => {
+                    expect(res).to.have.status([200]);
+                    expect(res.body).to.have.property('status');
+                done(error);
+
+                })
+        });
+        it('It should get an article if is not exist', (done) =>{
+            
+            chai.request(app)
+                .get('/api/v1/blogs/61')
+                .end((error, res) => {
+                    expect(res).to.have.status([404]);
+                    expect(res.body).to.have.property('status');
+                done(error);
+
+                })
+        });
+        
+        it('It should get all articles', (done) =>{
+            const id = '5f687da09ea6421941ecfd38'
+            chai.request(app)
+                .get('/api/v1/blogs/id')
+                
+                .end((error, res) => {
+                    expect(res).to.have.status([404]);
+                    expect(res.body).to.have.property('status');
+                done(error);
+
+                })
+        });
+
+        it('It should delete a articles if is not admin', (done) =>{
+            const id = '5f687da09ea6421941ecfd38'
+            chai.request(app)
+                .delete('/api/v1/blogs/id')
+                .set('Authorization', `Bearer ${damyData.userToken}`)
+                .end((error, res) => {
+                    expect(res).to.have.status([403]);
+                done(error);
+
+                })
+        });
+        it('It should delete a articles if is not admin', (done) =>{
+            
+            chai.request(app)
+                .delete('/api/v1/blogs/2')
+                .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+                .end((error, res) => {
+                    expect(res).to.have.status([404]);
+                    expect(res.body).to.have.property('message');
+                done(error);
+
+                })
+        });
+
+})
