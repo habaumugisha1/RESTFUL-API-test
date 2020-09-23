7e09b52e1eff25714e074b0ea9562781655a9c04
andela <andela@andelas-MacBook-Pro.local>
2020-09-23 20:54:31 +0200

    calling done method

--- API/tests/skills.test.js
+++ API/tests/skills.test.js
@@ -8,19 +8,19 @@ import damyData from './damyData'
 use(chaiHttp)
 
 describe('The skills test', ()=>{
-    it("it should not create skills if user is not admin", async () => {
-       const res = await request(app)
+    it("it should not create skills if user is not admin", (done) => {
+       request(app)
          .post('/api/v1/addSkills')
          .set('Authorization', `Bearer ${damyData.userToken}`)
          .send({neme:"HTML", skillImage:"hhfksafhaskhflaskdhfalk"})
-        //  .then((res) =>{
+         .then((res) =>{
             console.log(res.body)
-        // expect(res).have.status([403]);
+        expect(res).have.status([403]);
         expect(res.body).have.property("message");
         expect(res.body.message).to.equals("Only admin is allowed")
-        //  done()
-        //  })
-        //  .catch((err) => done(err))
+         done()
+         })
+         .catch((err) => done(err))
     })
 
      it("it should not create skills without name", (done) => {
@@ -45,31 +45,32 @@ describe('The skills test', ()=>{
          })
     })
 
-    it("it should create skills without error", async () => {
-      const res = await request(app)
+    it("it should create skills without error", (done) => {
+        request(app)
          .post('/api/v1/addSkills')
          .set('Authorization', `Bearer ${damyData.userAdminToken}`)
          .set('Content-Type', 'multipart/form-data')
          .field("name","mongoDb")
          .attach("skillImage", fs.readFileSync("images/how I get into Andela.jpg"), "how I get into Andela.jpg")
-        //  .then(res =>{
-        // expect(res).have.status([201])
+         .then(res =>{
+        expect(res).have.status([201])
         console.log(res.body)
         expect(res.body).have.property("data")
         expect(res.body).have.property("message")
              
-        //  done()
-        //  })
-        //  .catch((err) => done(err))
+         done()
+         })
+         .catch((err) => done(err))
     })
-    it("it should get all  skills ", async () => {
-       const res = await request(app)
+    it("it should get all  skills ", (done) => {
+       request(app)
          .get('/api/v1/skills')
-        //  .then(res =>{
+         .then(res =>{
+             console.log(res.body)
         expect(res).have.status([200])
         expect(res.body).have.property("data")
-        //  done()
-        //  })
-        //  .catch((err) => done(err))
+         done()
+         })
+         .catch((err) => done(err))
     })
 });
