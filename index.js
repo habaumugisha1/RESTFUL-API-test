8add0494f9ef8fc60fa1007c68ffebc6230e8ce3
andela <andela@andelas-MacBook-Pro.local>
2020-09-24 11:01:24 +0200

    mockgoose

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import bodyParser from "body-parser"
 import mongoose from 'mongoose'
-import { Mockngoose } from 'mongoose'
+import { Mockgoose } from 'mockgoose'
 import fileupload from 'express-fileupload'
 import "dotenv/config"
 import myRouter from './API/router/routes'
@@ -19,8 +19,8 @@ const url = process.env.DB_CONFIG;
 app.use('/api/v1', myRouter)
 
 if(process.env.NODE_ENV === 'test'){
-const Mockngoose = new Mockngoose(mongoose);
-mockngoose.prepareStorage().then(() => {
+const Mockgoose = new Mockgoose(mongoose);
+mockgoose.prepareStorage().then(() => {
     mongoose.connect( url, { useNewUrlParser: true,  useUnifiedTopology: true}).then( () =>{
     console.log("connected to mongodb")
 });
