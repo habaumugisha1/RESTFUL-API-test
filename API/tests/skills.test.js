054bdc82d6539263936db202fe841b4f962fb604
andela <andela@andelas-MacBook-Pro.local>
2020-09-24 08:35:12 +0200

    adding before and after in test file

--- API/tests/skills.test.js
+++ API/tests/skills.test.js
@@ -2,25 +2,31 @@ process.env.NODE_ENV ='test'
 import {use, request, expect} from 'chai'
 import chaiHttp from 'chai-http'
 import fs from 'fs'
+import mongoose from 'mongoose'
 import app from '../../index'
 import damyData from './damyData'
 
 use(chaiHttp)
-
+const url = process.env.DB_CONFIG;
 describe('The skills test', ()=>{
+    before((done) => {
+        mongoose.connect( url, { useNewUrlParser: true,  useUnifiedTopology: true}).then( () => done())
+        .catch((err) => done(err))
+    })
+
+    after((done) => {
+        mongoose.disconnect( url, { useNewUrlParser: true,  useUnifiedTopology: true}).then( () => done())
+        .catch((err) => done(err))
+    })
+
     it("it should not create skills if user is not admin", async() => {
         const res = await request(app)
          .post('/api/v1/addSkills')
          .set('Authorization', `Bearer ${damyData.userToken}`)
          .send({neme:"HTML", skillImage:"hhfksafhaskhflaskdhfalk"})
-        //  .then((res) =>{
-            console.log(res.body)
         expect(res).have.status([403]);
         expect(res.body).have.property("message");
         expect(res.body.message).to.equals("Only admin is allowed")
-        //  done()
-        //  })
-        //  .catch((err) => done(err))
     })
 
      it("it should not create skills without name", async () => {
@@ -28,23 +34,16 @@ describe('The skills test', ()=>{
          .post('/api/v1/addSkills')
          .set('Authorization', `Bearer ${damyData.userAdminToken}`)
          .send({skillImage:"hhfksafhaskhflaskdhfalk"})
-        //  .end((err, res) =>{
-            console.log(res.body)
           expect(res).have.status([400])
-        // done(err)
-        //  })
     })
+
     it("it should not create skills without token", async () => {
         const res = await request(app)
          .post('/api/v1/addSkills')
          .set('Authorization', `Bearer ${damyData.emptyToken}`)
          .send({name:"make tokens",skillImage:"hhfksafhaskhflaskdhfalk"})
-        //  .end((err, res) =>{
-            console.log(res.body)
              expect(res).have.status([400])
              expect(res.body).have.property("error")
-        // done(err)
-        //  })
     })
 
     it("it should create skills without error", async () => {
@@ -54,25 +53,16 @@ describe('The skills test', ()=>{
          .set('Content-Type', 'multipart/form-data')
          .field("name","mongoDb")
          .attach("skillImage", fs.readFileSync("images/how I get into Andela.jpg"), "how I get into Andela.jpg")
-        //  .then(res =>{
-            console.log(res.body)
         expect(res).have.status([201])
         expect(res.body).have.property("data")
         expect(res.body).have.property("message")
-             
-        //  done()
-        //  })
-        //  .catch((err) => done(err))
     })
+
     it("it should get all  skills ", async () => {
        const res = await request(app)
          .get('/api/v1/skills')
-        //  .then(res =>{
-             console.log(res.body)
         expect(res).have.status([200])
         expect(res.body).have.property("data")
-        //  done()
-        //  })
-        //  .catch((err) => done(err))
+        
     })
 });
