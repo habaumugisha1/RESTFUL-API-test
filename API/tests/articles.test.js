48211db345e16cd217ef21466304cd8ff41fea14
andela <andela@andelas-MacBook-Pro.local>
2020-09-29 18:26:48 +0200

    increase coverage

--- API/tests/articles.test.js
+++ API/tests/articles.test.js
@@ -4,6 +4,7 @@ import chaiHttp from 'chai-http';
 import fs from 'fs'
 import app from '../../index'
 import damyData from './damyData'
+import Articles from "../models/article"
 
 chai.use(chaiHttp)
 
@@ -19,6 +20,34 @@ it("should not create article if no title provided", (done) => {
         done(error)
     })
 })
+
+it("should create article", (done) => {
+    chai.request(app)
+    .post('/api/v1/newArticle')
+    .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+    .field('title', 'franklin')
+    .attach('articleImage',fs.readFileSync('images/how I get into Andela.jpg'), 'how I get into Andela.jpg')
+    .field('description', 'testing files')
+    .end((error, res) => {
+        expect(res).to.have.status([201]);
+        done(error)
+    })
+})
+
+it('It should get all articles', (done) =>{
+            
+            chai.request(app)
+                .get('/api/v1/blogs')
+                .end((error, res) => {
+                    expect(res).to.have.status([200]);
+                    expect(res.body).to.have.property('message');
+                    expect(res.body.message).to.equal("The blogs fetched");
+                    expect(res.body).to.have.property('data');
+                done(error);
+
+                })
+        });
+
 it('It should get an article if is not exist', (done) =>{
             
             chai.request(app)
@@ -31,6 +60,111 @@ it('It should get an article if is not exist', (done) =>{
                 })
         });
         
-       
+    
+it('It should get single article ', (done) =>{
+            const article = new Articles ({
+                    title: 'franklin',
+                    articleImage:'images/how I get into Andela.jpg',
+                    description: 'testing files'
+                        })
+                        article.save( (err, art) => {
+
+                    chai.request(app)
+                        .get(`/api/v1/blogs/${art.id}`)
+                        .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+                        .end((error, res) => {
+                            expect(res).to.have.status([200]);
+                            expect(res.body).to.have.property('status');
+                        done(error);
+
+                        })
+                })
+        });
+        
+
+it('It should not delete single article if not found', (done) =>{
+            const article = new Articles ({
+                    title: 'franklin',
+                    articleImage:'images/how I get into Andela.jpg',
+                    description: 'testing files'
+                        })
+                        article.save( (err, art) => {
+
+                   chai.request(app)
+                        .delete(`/api/v1/blogs/6`)
+                        .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+                        .end((error, res) => {
+                            expect(res).to.have.status([404]);
+                            expect(res.body).to.have.property('status');
+                        done(error);
+
+                        })
+                })
+        });
+
+    it('It should delete single article ', (done) =>{
+            const article = new Articles ({
+                    title: 'this is delete',
+                    articleImage:'images/how I get into Andela.jpg',
+                    description: 'testing files'
+                        })
+                        article.save( (err, art) => {
+
+                    chai.request(app)
+                        .delete(`/api/v1/blogs/${art.id}`)
+                        .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+                        .end((error, res) => {
+                            expect(res).to.have.status([200]);
+                            expect(res.body).to.have.property('status');
+                        done(error);
+
+                        })
+                })
+        });
+
+it('It should not update single article if not found ', (done) =>{
+            const article = new Articles ({
+                    title: 'this is delete',
+                    articleImage:'how I get into Andela.jpg',
+                    description: 'testing files'
+                        })
+                    article.save( (err, art) => {
+
+                    chai.request(app)
+                        .put(`/api/v1/blogs/7/edit`)
+                        .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+                        .end((error, res) => {
+                            expect(res).to.have.status([404]);
+                            expect(res.body).to.have.property('status');
+                        done(error);
+
+                        })
+                })
+        });
+    // it('It should update single article ', (done) =>{
+    //         const article = new Articles ({
+    //                 title: 'this is update article ',
+    //                 articleImage:'how I get into Andela.jpg',
+    //                 description: 'testing files'
+    //                     })
+    //                 article.save( (err, art) => {
+
+    //                 chai.request(app)
+    //                     .put(`/api/v1/blogs/${art.id}/edit`)
+    //                     .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+    //                     .send({
+    //                         title: 'this is update article on portfolio ',
+    //                         articleImage:'how I get into Andela.jpg',
+    //                         description: 'testing files'
+    //                             })
+    //                     .end((error, res) => {
+    //                         console.log(res)
+    //                         expect(res).have.status([200]);
+    //                         expect(res.body).have.property('message');
+    //                     done(error);
+
+    //                     })
+    //             })
+    //     });
 
 })
--- API/tests/skills.test.js
+++ API/tests/skills.test.js
@@ -6,6 +6,7 @@ import mongoose from 'mongoose'
 import "dotenv/config"
 import app from '../../index'
 import damyData from './damyData'
+import Skills from '../models/skill'
 
 use(chaiHttp)
 const uri = process.env.DB_CONFIG;
@@ -19,6 +20,81 @@ describe('The skills test', ()=>{
     //     mongoose.disconnect( uri, { useNewUrlParser: true,  useUnifiedTopology: true}).then( () => done())
     //     .catch((err) => done(err))
     // })
+    it("it should get a single skill", async () => {
+        let skill = new Skills({name:"communication", skillImage:"dfjhgdfgadjfgjadgf"})
+        skill.save( async (err, data) => {
+
+       const res = await request(app)
+         .get(`/api/v1/skills/${skill.id}`)
+        expect(res).have.status([200])
+        expect(res.body).have.property("results")
+        })
+        
+    })
+    
+    it("it should not delete a single skill if not exist", async () => {
+        let skill = new Skills({name:"communication", skillImage:"dfjhgdfgadjfgjadgf"})
+        await skill.save( async (err, data) => {
+
+       const res = await request(app)
+         .delete(`/api/v1/skill/2/delete`)
+         .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+         console.log(data)
+        expect(res).have.status(404)
+        expect(res.body).have.property("message")
+        })
+        
+    })
+
+    it("it should not update a single skill if not exist", async () => {
+        let skill = new Skills({name:"communication", skillImage:"dfjhgdfgadjfgjadgf"})
+        await skill.save( async (err, data) => {
+       const res = await request(app)
+         .patch(`/api/v1/skills/3/edit`)
+         .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+        expect(res).have.status([404])
+        expect(res.body).have.property("message")
+        })
+        
+    })
+    it("it should  update a single skill if exist", async () => {
+        let skill = new Skills({name:"communication", skillImage:"dfjhgdfgadjfgjadgf"})
+        await skill.save( async (err, data) => {
+       const res = await request(app)
+         .patch(`/api/v1/skills/${data.id}/edit`)
+         .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+         .send({name:"communication skills"})
+        expect(res).have.status([200])
+        expect(res.body).have.property("message")
+        })
+        
+    })
+    it("it should  update image of a single skill if exist", async () => {
+        let skill = new Skills({name:"communication", skillImage:"dfjhgdfgadjfgjadgf"})
+        await skill.save( async (err, data) => {
+       const res = await request(app)
+         .patch(`/api/v1/skills/${data.id}/edit`)
+         .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+         .attach("skillImage", fs.readFileSync("images/how I get into Andela.jpg"), "how I get into Andela.jpg")
+
+        expect(res).have.status([200])
+        expect(res.body).have.property("message")
+        })
+        
+    })
+
+it("it should  delete a single skill if exist", async () => {
+        let skill = new Skills({name:"communication", skillImage:"dfjhgdfgadjfgjadgf"})
+        await skill.save( async (err, data) => {
+       const res = await request(app)
+         .delete(`/api/v1/skills/${data.id}/delete`)
+         .set('Authorization', `Bearer ${damyData.userAdminToken}`)
+         .attach("skillImage", fs.readFileSync("images/how I get into Andela.jpg"), "how I get into Andela.jpg")
+        expect(res).have.status([200])
+        expect(res.body).have.property("message")
+        })
+        
+    })
 
     it("it should not create skills if user is not admin", async() => {
         const res = await request(app)
@@ -66,4 +142,18 @@ describe('The skills test', ()=>{
         expect(res.body).have.property("data")
         
     })
+     
+     it("it should get a single skill", async () => {
+        let skill = new Skills({name:"communication", skillImage:"dfjhgdfgadjfgjadgf"})
+        skill.save( async (err, data) => {
+
+       const res = await request(app)
+         .get(`/api/v1/skills/10`)
+        expect(res).have.status([404])
+        expect(res.body).have.property("message")
+        })
+        
+    })
+    
+    
 });
